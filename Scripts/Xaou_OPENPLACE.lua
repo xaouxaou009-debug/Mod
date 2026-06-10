37f97b1ceadaf363ee73c3ab2f9f17d6e5be792d
xaouxaou009-debug <xaouxaou009@gmail.com>
2026-06-10 11:12:59 +0700

    Add files via upload

---
+++ Scripts/Xaou_OPENPLACE.lua
@@ -0,0 +1,14 @@
+﻿local Xaou_xaou009 = GameMain:GetMod("Xaou_xaou009");
+local time = 0;
+local flag = 0;
+
+function Xaou_xaou009:OnStep(dt)
+if flag == 0 then
+time = time + dt;
+if time >= 10  then
+flag = 1;
+CS.XiaWorld.PlacesMgr.Instance:UnlockAll()
+end
+end
+end
+
